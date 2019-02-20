336ace01a0089d9641ad01b595dda112684b7964
Administrator <saad.thaifa@karizma.ma>
2019-02-20 17:24:38 +0000

    Update __manifest__.py

--- kzm_backup/__manifest__.py
+++ kzm_backup/__manifest__.py
@@ -2,16 +2,13 @@
 {
     'name': "kzm_backup",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Manage Odoo Instance backup"
 
     'description': """
-        Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "KARIZMA CONSEIL",
+    'website': "http://www.karizma.ma",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml

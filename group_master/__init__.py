f87b5f3b169c2fdfc722a13faf315bdc5163b89b
reda <r.rouichi@karizma.ma>
2019-05-06 11:20:53 +0000

    Update __init__.py

--- group_master/__init__.py
+++ group_master/__init__.py
@@ -20,8 +20,6 @@
 ##############################################################################
 
 from . import ir_model
-from . import test
-
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

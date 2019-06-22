64b6f3360644dbd7ed3acb9ba60cb2b8907800ab
Administrator <saad.thaifa@karizma.ma>
2019-06-22 21:14:02 +0000

    Mettre à jour kzm_project_activity/models/project_project.py

--- kzm_project_activity/models/project_project.py
+++ kzm_project_activity/models/project_project.py
@@ -6,6 +6,6 @@ from odoo import models, fields, api, exceptions, _
 class ProjectProject(models.Model):
 
     _name = 'project.project'
-    _inherit = ['project.project', 'mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'utm.mixin']
 
     #activity_ids = fields.Many2many('mail.activity.mixin', string="Project activities")

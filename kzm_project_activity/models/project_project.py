fe0d5a3f71873184ea733fd27943e76a6d3c6fb9
Administrator <saad.thaifa@karizma.ma>
2019-06-22 21:03:03 +0000

    Update project_project.py

--- kzm_project_activity/models/project_project.py
+++ kzm_project_activity/models/project_project.py
@@ -8,8 +8,4 @@ class ProjectProject(models.Model):
     _name = 'project.project'
     _inherit = ['project.project', 'mail.activity.mixin']
 
-    # activity_ids = fields.Many2many('mail.activity.mixin', string="Project activities")
-
-
-class MailActivity(models.Model):
-    _inherit = 'mail.activity'
+    activity_ids = fields.Many2many('mail.activity.mixin', string="Project activities")

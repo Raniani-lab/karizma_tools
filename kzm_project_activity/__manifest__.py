76eed792d3420292808c6ed4678a6d0949e91049
Assabe <assabepolo@karizma-conseil.com>
2019-06-25 14:14:28 +0100

    [ADD] activity project

---
+++ kzm_project_activity/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "KZM Project Activity",
+    'summary': """
+        Add activity to an project""",
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "KARIZMA Conseil",
+    'website': "http://karizma-conseil.com/",
+    'category': 'Uncategorized',
+    'version': '11.0',
+    'depends': ['base',
+                'project',
+                ],
+    'data': [
+        'views/project_view.xml',
+    ],
+}

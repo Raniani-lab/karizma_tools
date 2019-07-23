6daf3e21e3d482d1aed1a898fd05355c8b276602
Abdelmajid <a.elhamdaoui@karizma.ma>
2019-07-23 14:26:12 +0100

    [FIX] AUTO-BACKUP SFTP OCA

---
+++ oca_auto_backup_extension/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "OCA AUTO-BACKUP, KZM EXTENSION",
+    "version": "0.2.3",
+    "author": "Abdelmajid Elhamdaoui, KARIZMA CONSEIL",
+    "license": 'AGPL-3',
+    "description": """
+      Nedd OCA/SEREVER-TOOLS
+    """,
+    "summary": "",
+    "website": "http://www.karizma.ma",
+    "category": 'Tools',
+    "sequence": 20,
+    "depends": [
+        'auto_backup',
+    ],
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
---
+++ oca_auto_backup_extension/models/models.py
@@ -0,0 +1,48 @@
+# © 2004-2009 Tiny SPRL (<http://tiny.be>).
+# © 2015 Agile Business Group <http://www.agilebg.com>
+# © 2016 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import logging
+import os
+import shutil
+import traceback
+from contextlib import contextmanager
+from datetime import datetime, timedelta
+from glob import iglob
+
+from odoo import _, api, exceptions, fields, models, tools
+from odoo.service import db
+
+_logger = logging.getLogger(__name__)
+try:
+    import pysftp
+except ImportError:  # pragma: no cover
+    _logger.debug('Cannot import pysftp')
+
+class DbBackup(models.Model):
+    _inherit = 'db.backup'
+
+    @api.multi
+    def sftp_connection(self):
+        """Return a new SFTP connection with found parameters."""
+        self.ensure_one()
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        params = {
+            "host": self.sftp_host,
+            "username": self.sftp_user,
+            "port": self.sftp_port,
+            "cnopts": cnopts,
+        }
+        _logger.debug(
+            "Trying to connect to sftp://%(username)s@%(host)s:%(port)d",
+            extra=params)
+        if self.sftp_private_key:
+            params["private_key"] = self.sftp_private_key
+            if self.sftp_password:
+                params["private_key_pass"] = self.sftp_password
+        else:
+            params["password"] = self.sftp_password
+
+        return pysftp.Connection(**params)
